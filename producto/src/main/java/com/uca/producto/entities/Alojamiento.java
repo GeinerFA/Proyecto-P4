3255217cc976e5b2fc6716e523cbb0044590f89c
Geiner_FA <geinerfa@gmail.com>
2024-11-29 23:15:01 -0600

    Alojamiento y cliente listos

---
+++ producto/src/main/java/com/uca/producto/entities/Alojamiento.java
@@ -0,0 +1,115 @@
+package com.uca.producto.entities;
+import java.util.Date;
+
+public class Alojamiento {
+    private int idAlojamiento;
+    private String nombre;
+    private int cantidadPersonas;
+    private Date fecha_ingreso;
+    private Date fecha_salida;
+    private int precio;
+    private String ubicacion;
+    private boolean estado;
+    private int idDestino;
+    private int idPais;
+
+    public int getidAlojamiento() {
+        return idAlojamiento;
+    }
+
+    public void setidAlojamiento(int idAlojamiento) {
+        this.idAlojamiento = idAlojamiento;
+    }
+
+    public String getnombre() {
+        return nombre;
+    }
+
+    public void setnombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getcantidadPersonas(){
+        return cantidadPersonas;
+    }
+    
+    public void setcantidadPersonas(int cantidadPersonas) {
+        this.cantidadPersonas = cantidadPersonas;
+    }
+
+    public Date getfecha_ingreso(){
+        return fecha_ingreso;
+    }
+
+    public void setfecha_ingreso(Date fecha_ingreso) {
+        this.fecha_ingreso = fecha_ingreso;
+    }
+    
+    public Date getfecha_salida(){
+        return fecha_salida;
+    }
+
+    public void setfecha_salida(Date fecha_salida) {
+        this.fecha_salida = fecha_salida;
+    }
+
+    public int getprecio() {
+        return precio;
+    }
+
+    public void setprecio(int precio) {
+        this.precio = precio;
+    }
+
+    public String getubicacion() {
+        return ubicacion;
+    }
+
+    public void setubicacion(String ubicacion) {
+        this.ubicacion = ubicacion;
+    }
+
+    public boolean getestado() {
+        return estado;
+    }
+
+    public void setestado(boolean estado) {
+        this.estado = estado;
+    }
+
+    public Alojamiento() {
+    }
+
+    public Alojamiento(int idAlojamiento) {
+        this.idAlojamiento = idAlojamiento;
+    }
+
+    public int getidDestino() {
+        return idDestino;
+    }
+
+    public void setidDestino(int idDestino) {
+        this.idDestino = idDestino;
+    }
+
+    public int getidPais() {
+        return idPais;
+    }
+
+    public void setidPais(int idPais) {
+        this.idPais = idPais;
+    }
+
+    public Alojamiento(int idAlojamiento, String nombre, int cantidadPersonas, Date fecha_ingreso, Date fecha_salida, int precio, String ubicacion,boolean estado, int idDestino, int idPais) {
+        this.idAlojamiento = idAlojamiento;
+        this.nombre = nombre;
+        this.cantidadPersonas = cantidadPersonas;
+        this.fecha_ingreso = fecha_ingreso;
+        this.fecha_salida = fecha_salida;
+        this.precio = precio;
+        this.ubicacion = ubicacion;
+        this.estado = estado;
+        this.idDestino = idDestino;
+        this.idPais = idPais;
+    }
+}
---
+++ producto/src/main/java/com/uca/producto/logic/LAlojamiento.java
@@ -0,0 +1,137 @@
+package com.uca.producto.logic;
+
+import java.sql.ResultSet;
+import java.sql.Types;
+import java.util.ArrayList;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import com.uca.producto.db.ConnectionManager;
+import com.uca.producto.db.TParametro;
+import com.uca.producto.entities.Alojamiento;
+
+public class LAlojamiento {
+    public ArrayList<Alojamiento> Listar() {
+        ArrayList<Alojamiento> alojamientos = new ArrayList<>();
+
+        // Definir y cargar los parámetros.
+        ArrayList<TParametro<?>> parametros = new ArrayList<>();
+        parametros.add(new TParametro<>("p_respuesta", null, Types.REF_CURSOR, true));
+
+        try (ConnectionManager cm = new ConnectionManager()) {
+            if (cm.Connect()) {
+                try (ResultSet rs = cm.Execute("proyecto.operaciones.sp_op_listar_alojamiento(?)", parametros)) {
+                    while (rs.next()) {
+                        alojamientos.add(new Alojamiento(
+                                rs.getInt("idAlojamiento"),
+                                rs.getString("nombre"),
+                                rs.getInt("cantidadPersonas"),
+                                rs.getDate("fecha_ingreso"),
+                                rs.getDate("fecha_salida"),
+                                rs.getInt("precio"),
+                                rs.getString("ubicacion"),
+                                rs.getBoolean("estado"),
+                                rs.getInt("idDestino"),
+                                rs.getInt("idAlojamiento")
+                        ));
+                    }
+                }
+                catch (Exception ex) {
+                    Logger.getLogger(LAlojamiento.class.getName()).log(Level.SEVERE, null, ex);
+                    return null;
+
+                }
+            }
+            return alojamientos;
+        } catch (Exception ex) {
+            Logger.getLogger(LAlojamiento.class.getName()).log(Level.SEVERE, null, ex);
+            return null;
+        }
+    }
+
+    public Alojamiento Consultar(int idAlojamiento) {
+        Alojamiento alojamiento = null;
+
+        ArrayList<TParametro<?>> parametros = new ArrayList<>();
+        parametros.add(new TParametro<>("p_id_alojamiento", idAlojamiento, Types.NUMERIC));
+        parametros.add(new TParametro<>("p_respuesta", null, Types.REF_CURSOR, true));
+
+        try (ConnectionManager cm = new ConnectionManager()) {
+            if (cm.Connect()) {
+                try (ResultSet rs = cm.Execute("proyecto.operaciones.sp_op_consultar_alojamiento(?,?)", parametros)) {
+                    if (rs.next()) {
+                        // Cargamos la información del pais
+                        alojamiento = new Alojamiento(
+                            rs.getInt("idAlojamiento"),
+                                rs.getString("nombre"),
+                                rs.getInt("cantidadPersonas"),
+                                rs.getDate("fecha_ingreso"),
+                                rs.getDate("fecha_salida"),
+                                rs.getInt("precio"),
+                                rs.getString("ubicacion"),
+                                rs.getBoolean("estado"),
+                                rs.getInt("idDestino"),
+                                rs.getInt("idAlojamiento")
+                        );
+
+                    }
+                }
+            }
+            return alojamiento;
+        } catch (Exception ex) {
+            Logger.getLogger(LAlojamiento.class.getName()).log(Level.SEVERE, null, ex);
+            return null;
+        }
+    }
+
+    public int Guardar(Alojamiento alojamiento) {
+        // Definir y cargar los parámetros.
+        ArrayList<TParametro<?>> parametros = new ArrayList<>();
+        
+        parametros.add(new TParametro<>("p_nombre", alojamiento.getnombre(), Types.VARCHAR));
+        parametros.add(new TParametro<>("cantidadPersonas", alojamiento.getcantidadPersonas(), Types.VARCHAR));
+        parametros.add(new TParametro<>("fecha_ingreso", alojamiento.getfecha_ingreso(), Types.VARCHAR));
+        parametros.add(new TParametro<>("fecha_salida", alojamiento.getfecha_salida(), Types.VARCHAR));
+        parametros.add(new TParametro<>("precio", alojamiento.getprecio(), Types.VARCHAR));
+        parametros.add(new TParametro<>("ubicacion", alojamiento.getubicacion(), Types.VARCHAR));
+        parametros.add(new TParametro<>("estado", alojamiento.getestado(), Types.VARCHAR));
+        parametros.add(new TParametro<>("idDestino", alojamiento.getidDestino(), Types.VARCHAR));
+        parametros.add(new TParametro<>("idPais", alojamiento.getidPais(), Types.VARCHAR));
+        parametros.add(new TParametro<>("p_respuesta", null, Types.INTEGER, true));
+
+        try (ConnectionManager cm = new ConnectionManager()) {
+            if (cm.Connect()) {
+                return cm.Execute("proyecto.operaciones.sp_op_guardar_alojamiento(?,?,?)", parametros);
+            }
+            return 0;
+        } catch (Exception e) {
+            Logger.getLogger(ConnectionManager.class.getName()).log(Level.SEVERE, null, e);
+            return -1;
+        }
+    }
+
+    public int Actualizar(Alojamiento alojamiento) {
+        // Definir y cargar los parámetros.
+        ArrayList<TParametro<?>> parametros = new ArrayList<>();
+        parametros.add(new TParametro<>("p_nombre", alojamiento.getnombre(), Types.VARCHAR));
+        parametros.add(new TParametro<>("cantidadPersonas", alojamiento.getcantidadPersonas(), Types.VARCHAR));
+        parametros.add(new TParametro<>("fecha_ingreso", alojamiento.getfecha_ingreso(), Types.VARCHAR));
+        parametros.add(new TParametro<>("fecha_salida", alojamiento.getfecha_salida(), Types.VARCHAR));
+        parametros.add(new TParametro<>("precio", alojamiento.getprecio(), Types.VARCHAR));
+        parametros.add(new TParametro<>("ubicacion", alojamiento.getubicacion(), Types.VARCHAR));
+        parametros.add(new TParametro<>("estado", alojamiento.getestado(), Types.VARCHAR));
+        parametros.add(new TParametro<>("idDestino", alojamiento.getidDestino(), Types.VARCHAR));
+        parametros.add(new TParametro<>("idPais", alojamiento.getidPais(), Types.VARCHAR));
+        parametros.add(new TParametro<>("p_respuesta", null, Types.INTEGER, true));
+
+        try (ConnectionManager cm = new ConnectionManager()) {
+            if (cm.Connect()) {
+                return cm.Execute("proyecto.operaciones.sp_op_actualizar_alojamiento(?,?,?,?)", parametros);
+            }
+            return 0;
+        } catch (Exception e) {
+            Logger.getLogger(ConnectionManager.class.getName()).log(Level.SEVERE, null, e);
+            return -1;
+        }
+    }
+}
